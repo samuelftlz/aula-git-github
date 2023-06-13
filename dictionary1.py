d2b22f8668945922e66b26dd1cf23e7fd4c4cd0f
samuelftlz <134955854+samuelftlz@users.noreply.github.com>
2023-06-13 15:57:56 -0300

    Delete dictionary1.py

--- dictionary1.py
+++
@@ -1,9 +0,0 @@
-aluno = {}
-aluno['nome'] = input('digite o nome: ')
-aluno['media'] = float(input(f'media do {aluno["nome"]}: '))
-
-if (aluno['media'] >= 7):
-    print('aprovado')
-else:
-    print('reprovado')
-print(aluno)
